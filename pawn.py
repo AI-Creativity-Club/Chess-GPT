6108d73b3025f362fe19af0981673cc166299282
MrAwesomePants <nicholassenachan@gmail.com>
2023-05-12 23:29:13 -0700

    Added pawn advance on first move

--- pawn.py
+++ pawn.py
@@ -8,6 +8,8 @@ CLASS: Pawn
 DESC: defines a pawn that will be draw on the scene
 """
 class Pawn:
+    
+    
     """
     FUNC: __init__
     DESC: stores information on the sprite
@@ -19,6 +21,8 @@ class Pawn:
         self.color = color
         self.sprite = pygame.image.load(sprite_path)
         self.sprite = pygame.transform.scale(self.sprite, (60, 60))
+        self.first_move = 0
+
     """
     FUNC: draw
     DESC: draws the sprite on the board/scene
@@ -32,7 +36,7 @@ class Pawn:
     I/O: self:obj, dx:int, dy:int, board_state:2d array | bool
     """
     def move(self, dx, dy, board_state):
-
+        self.first_move += 1
         new_x = self.x + dx
         new_y = self.y + dy
         if new_x < 0 or new_x >= 8 or new_y < 0 or new_y >= 8:
@@ -45,6 +49,12 @@ class Pawn:
                     self.x, self.y = new_x, new_y
                     board_state[self.y][self.x] = self  # Update the new square with the piece
                     return True
+            if dx == 0 and dy == (2 if self.color == "white" else -2) and self.first_move == 1:
+                board_state[self.y][self.x] = None  # Update the old square to be empty
+                self.x, self.y = new_x, new_y
+                board_state[self.y][self.x] = self  # Update the new square with the piece
+                return True
+                        
             elif abs(dx) == 1 and dy == (1 if self.color == "white" else -1):
                 if target_piece is not None and target_piece.color != self.color:
                     board_state[self.y][self.x] = None  # Update the old square to be empty
